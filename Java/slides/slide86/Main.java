897eff468571acb6298f5e4420f44123425797aa
dimjim2 <45967416+dimjim2@users.noreply.github.com>
2020-03-24 18:28:47 +0200

    Add files via upload

---
+++ Java/slides/slide86/Main.java
@@ -0,0 +1,10 @@
+package com.dimmy;
+
+public class Main {
+    public static void main(String... args) {
+        Test.Exercice te = new Test.Exercice();
+        te.display();
+        te.solve();
+        te.printMessage();
+    }
+}
---
+++ Java/slides/slide86/Solve.java
@@ -0,0 +1,7 @@
+package com.dimmy;
+
+public interface Solve {
+     static void solve(){
+        System.out.println("I am solving an exercise");
+    };
+}
